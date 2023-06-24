e805527c038028d8d05fcda8e75552b254a7462d
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-24 12:27:21 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,73 @@
 
 import React from "react";
 import './../styles/App.css';
+import AutoComplete from "./AutoComplete";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Search item</h1>
+        <AutoComplete fruits={fruits}/>
     </div>
   )
 }
 
 export default App
+
+// import React, { useState } from 'react';
+
+// const Autocomplete = ({ suggestions }) => {
+//   const [inputValue, setInputValue] = useState('');
+//   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  
+//   const handleInputChange = (event) => {
+//     const userInput = event.target.value;
+//     setInputValue(userInput);
+    
+//     // Filter the suggestions based on the user input
+//     const filtered = suggestions.filter(
+//       (fruit) =>
+//         fruit.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+//     );
+//     setFilteredSuggestions(filtered);
+//   };
+  
+//   const handleSuggestionClick = (suggestion) => {
+//     setInputValue(suggestion);
+//     setFilteredSuggestions([]);
+//   };
+  
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={handleInputChange}
+//         placeholder="Search for a fruit..."
+//       />
+//       <ul>
+//         {filteredSuggestions.map((suggestion, index) => (
+//           <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+//             {suggestion}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig","orange","aam"];
+  
+//   return (
+//     <div>
+//       <h1>Autocomplete Example</h1>
+//       <Autocomplete suggestions={fruits} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
---
+++ src/components/AutoComplete.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const AutoComplete = ({ fruits }) => {
+  const [inputText, setinputText] = useState("");
+  const [filteredArray, setFilteredArray] = useState([]);
+
+  const handleInput = (e) => {
+    const input = e.target.value;
+    setinputText(input);
+
+    if (input.length > 0) {
+      const filtered = fruits.filter((item) =>
+        item.toLowerCase().startsWith(input.toLowerCase())
+      );
+      setFilteredArray(filtered);
+    }else{
+        setFilteredArray(fruits);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={handleInput}
+        value={inputText}
+        placeholder="Search ..."
+      />
+
+      <ul>
+        {filteredArray.map((val, i) => (
+          <li key={i}>{val}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AutoComplete;
